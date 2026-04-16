c322f654afd90c3b16d0199d67bdea5f0f393539
alexdolya08170-web <alexdolya08170@gmail.com>
2026-04-16 23:30:35 +0300

    feat: add React Compiler support via babel plugin

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,23 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import { babel } from '@rolldown/plugin-babel'
+import babel from '@rolldown/plugin-babel'
 
-// Конфигурация компилятора (опционально)
 const ReactCompilerConfig = {
-  
-  // Постепенное внедрение:
-  compilationMode: 'annotation', // 'all' | 'annotation' | 'infer'
-  
-  // Логирование для отладки:
-  // logger: { logEvent: (filename, event) => console.log(filename, event) }
+  compilationMode: 'annotation',
 }
 
 export default defineConfig({
   plugins: [
     babel({
       include: /\.[jt]sx?$/,
-      babelConfig: {
-        ...reactCompilerPreset(ReactCompilerConfig),
-        // Дополнительные плагины Babel при необходимости:
-        // plugins: ['@babel/plugin-proposal-throw-expressions'],
-      },
+      presets: [reactCompilerPreset(ReactCompilerConfig)],
     }),
     react(),
   ],
-  
-  // Дополнительные оптимизации
   build: {
-    sourcemap: true, // Обязательно для отладки скомпилированного кода
+    sourcemap: true,
     target: 'esnext',
   },
-  
-  //  Оптимизация зависимостей
   optimizeDeps: {
     esbuildOptions: {
       target: 'esnext',
